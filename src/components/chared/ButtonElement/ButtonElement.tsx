dfe85621698fa324584b6104d3815040125541dd
Evgeny Zherdev <zheny-ye@bk.ru>
2024-06-14 13:44:18 +0700

    измения в валидации(вынос галочки)

--- src/components/chared/ButtonElement/ButtonElement.tsx
+++ src/components/chared/ButtonElement/ButtonElement.tsx
@@ -1,11 +1,9 @@
 import styles from "./ButtonElement.module.scss"
 import {FC} from "react";
+import {ITitle} from "../../../types/type.ts";
 
-interface IProps {
-    title: string
-}
 
-const ButtonElement: FC<IProps> = ({title}) => {
+const ButtonElement: FC<ITitle> = ({title}) => {
     return (
         <div className={styles.button}>
             <button className={styles.button__container}>{title}</button>
